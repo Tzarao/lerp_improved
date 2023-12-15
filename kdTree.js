6697e20069894bbb9fb5b776a17d77c4b93fa7e4
Tzarao <152451184+Tzarao@users.noreply.github.com>
2023-12-15 22:02:23 +0100

    Update kdTree.js

--- kdTree.js
+++ kdTree.js
@@ -1,37 +1,31 @@
 const c = document.getElementById("canvas");
-const off = 8;
-const w = c.width =24;
-const h = c.height = 24;
-const colors = [[255,0,0], [0,0,255]];
-const getR = () => colors[Math.round((Math.random()))];
+const w = c.width = 2;
+const h = c.height= 2;
+const colors = [[0,255,0], [255,0,0]];
+//const getR = () => colors[Math.round((Math.random()))];
 const toRGBA = (rgba) => `rgba(${rgba[0]}, ${rgba[1]}, ${rgba[2]}, 1)`;
 const ctx = c.getContext("2d");
-let points = [];
-
-ctx.fillStyle = toRGBA(getR())
-ctx.fillRect(
-    off,
-    off,
-    w - (off * 2),
-    h - (off * 2)
-);
+const lerpFactor = 0.55;
+const points = [];
 
 for (let i = 0; i < w; i++) {
-    let row = []
-    for (let y = 0; y < h; y++) {
-        let color = getR();
-        if(i > 0 && points[i - 1][y]) {
-            const side = points[i - 1][y];
-            const n = [
-                (side[0] * (0.5)) + (color[0] * 0.5),
-                (side[1] * (0.5)) + (color[1] * 0.5),
-                (side[2] * (0.5)) + (color[2] * 0.5),
-                ];
-            ctx.fillStyle = toRGBA(n);
+    const row = []
 
-        } else {
-            ctx.fillStyle = toRGBA(color);
+    for (let y = 0; y < h; y++) {
+        let color = colors[i ^ y ? 1 : 0];
+
+        if (i > 0 && points[i - 1][y]) {
+            const side = points[i - 1][y];
+
+            const sideblend = [
+                (side[0] * (lerpFactor)) + (color[0] * lerpFactor),
+                (side[1] * (lerpFactor)) + (color[1] * lerpFactor),
+                (side[2] * (lerpFactor)) + (color[2] * lerpFactor),
+            ];
+
+            color = sideblend;
         }
+        ctx.fillStyle = toRGBA(color);
         ctx.fillRect(i,y,1,1);
         row.push(color);
     }
@@ -39,5 +33,7 @@ for (let i = 0; i < w; i++) {
     console.log(row);
     console.log(" -- ")
 }
+
+console.log(points)
 
 console.log(points)
