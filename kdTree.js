8af5ec3ac1ea647fbdce10de6cfa7ccdd9fdb58e
Tzarao <152451184+Tzarao@users.noreply.github.com>
2023-12-15 23:35:28 +0100

    Update kdTree.js

--- kdTree.js
+++ kdTree.js
@@ -1,22 +1,21 @@
 const c = document.getElementById("canvas");
-const w = c.width = 2;
-const h = c.height= 2;
-const colors = [[0,255,0], [255,0,0]];
-//const getR = () => colors[Math.round((Math.random()))];
+const w = c.width = 72;
+const h = c.height= 72;
+const colors = [[0,248,24], [0,0,224]];
+const getR = () => colors[Math.round((Math.random()))];
 const toRGBA = (rgba) => `rgba(${rgba[0]}, ${rgba[1]}, ${rgba[2]}, 1)`;
 const ctx = c.getContext("2d");
-const lerpFactor = 0.55;
 const points = [];
 
 for (let i = 0; i < w; i++) {
     const row = []
 
     for (let y = 0; y < h; y++) {
-        let color = colors[i ^ y ? 1 : 0];
+        const lerpFactor = 1 / 2;
+        let color = getR();
 
         if (i > 0 && points[i - 1][y]) {
             const side = points[i - 1][y];
-
             const sideblend = [
                 (side[0] * (lerpFactor)) + (color[0] * lerpFactor),
                 (side[1] * (lerpFactor)) + (color[1] * lerpFactor),
@@ -25,15 +24,14 @@ for (let i = 0; i < w; i++) {
 
             color = sideblend;
         }
+
         ctx.fillStyle = toRGBA(color);
         ctx.fillRect(i,y,1,1);
+
         row.push(color);
     }
-    points.push(row);
-    console.log(row);
-    console.log(" -- ")
-}
 
-console.log(points)
+    points.push(row);
+}
 
 console.log(points)
